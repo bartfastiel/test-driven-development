d72c4657ad19836b37c744ca1d93638d5daf2df9
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-18 10:05:12 +0200

    Refactor tests to parameterized test

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,83 +1,23 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class FizzBuzzTest {
 
-    @Test
-    void expect1_whenPerson1() {
-        // GIVEN
-        int person = 1;
-
-        // WHEN
+    @ParameterizedTest
+    @CsvSource(
+            delimiter = '=',
+            value = {
+                    "1=1",
+                    "2=2",
+                    "3=Fizz",
+                    "4=4",
+                    "5=Buzz",
+                    "15=Fizz Buzz"
+            }
+    )
+    void testAnswer(int person, String expected) {
         String actual = FizzBuzz.answer(person);
-
-        // THEN
-        String expected = "1";
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void expect2_whenPerson2() {
-        // GIVEN
-        int person = 2;
-
-        // WHEN
-        String actual = FizzBuzz.answer(person);
-
-        // THEN
-        String expected = "2";
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void expectFizz_whenPerson3() {
-        // GIVEN
-        int person = 3;
-
-        // WHEN
-        String actual = FizzBuzz.answer(person);
-
-        // THEN
-        String expected = "Fizz";
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void expect4_whenPerson4() {
-        // GIVEN
-        int person = 4;
-
-        // WHEN
-        String actual = FizzBuzz.answer(person);
-
-        // THEN
-        String expected = "4";
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void expectBuzz_whenPerson5() {
-        // GIVEN
-        int person = 5;
-
-        // WHEN
-        String actual = FizzBuzz.answer(person);
-
-        // THEN
-        String expected = "Buzz";
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void expectFizzBuzz_whenPerson15() {
-        // GIVEN
-        int person = 15;
-
-        // WHEN
-        String actual = FizzBuzz.answer(person);
-
-        // THEN
-        String expected = "Fizz Buzz";
         Assertions.assertEquals(expected, actual);
     }
 }
